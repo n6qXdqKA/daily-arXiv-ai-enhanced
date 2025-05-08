5b98b5a1f00f547f3b65654023ba9b774c1c8af7
n6qXdqKA <165235446+n6qXdqKA@users.noreply.github.com>
2025-05-08 10:28:52 +0800

    Update arxiv.py

--- daily_arxiv/daily_arxiv/spiders/arxiv.py
+++ daily_arxiv/daily_arxiv/spiders/arxiv.py
@@ -6,7 +6,7 @@ class ArxivSpider(scrapy.Spider):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # categories = os.environ.get("CATEGORIES", "cs.CV")
-        categories = "cs.CV,cs.AI"
+        categories = "cs.CV"
         categories = categories.split(",")
         categories = list(map(str.strip, categories))
         self.start_urls = [
